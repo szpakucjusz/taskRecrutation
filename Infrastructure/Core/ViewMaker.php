e86cdee35b2078f484069580196c13b7d4752848
szpakucjusz <lukasz.szpakowski2@gmail.com>
2021-12-29 00:39:23 +0100

    feature/add-display-data-template

--- Infrastructure/Core/ViewMaker.php
+++ Infrastructure/Core/ViewMaker.php
@@ -9,13 +9,13 @@ class ViewMaker
     /** @param array|null|mixed[] $parameters */
     public function render(string $templateName = '', ?array $parameters = []): string
     {
-        // Make values in the associative array easier to access by extracting them
         if ( is_array( $parameters ) ){
             extract( $parameters );
         }
 
         ob_start();
         include __DIR__ . '/../../templates/' . $templateName . '.php';
+
         return ob_get_clean();
     }
 }
